da0cad63ff33e637072d8b91942e2b0c00832046
Lauren Yu <laurenkyu@gmail.com>
2020-01-10 17:04:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,12 +6,12 @@ def using_unshift(array,string)
   array.unshift(string)
 end
 
-def using_pop
+def using_pop(array)
   array.pop
 end
 
-def pop_with_args
-  
+def pop_with_args(array)
+  array.pop(2)
 end
 
 def using_shift
